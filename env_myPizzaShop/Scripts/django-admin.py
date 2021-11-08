780d97bd39ca687d6d78c363e51d9455c3d98330
Anzor <anzors.andrejevs@gmail.com>
2021-11-08 16:02:04 +0000

    Cretated app file and html files

---
+++ env_myPizzaShop/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\anzor\source\repos\WebProgramming\PizzaShop\env_myPizzaShop\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
